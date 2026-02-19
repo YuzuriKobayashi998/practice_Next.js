3490054779e9cb26f3b4c67e0f3a5bac348d455e
Kobayashi yuzuri <oudon5963@gmail.com>
2026-02-20 00:12:34 +0900

    フロントエンド準備

--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request){
-    const token = await rewuest.headers.get("Authorization")?.split("")[1]
+    const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im91ZG9uQGhvZ2UuY29tIiwiZXhwIjoxNzcxNTk5MjIyfQ.HhQjNnAPmxzveV53yCSs_MJgKy7fBtvZvRRAqAJLvhk"
+    //await rewuest.headers.get("Authorization")?.split("")[1]
     if(!token){
         return NextResponse.json({message: "トークンがありません"})
     }
